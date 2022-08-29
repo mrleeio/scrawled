b993361a0d9449cf7e44774a15f2610349919973
Michael Lee <michael@mrlee.io>
2022-08-29 19:40:16 -0400

    Temporary homepage (#3)
    Modify the default index page to include basic contact details.
    
    This work is in prepartion of adding a deployment pipeline; I don't want
    to expose the default Pheonix page.

--- test/scrawled_web/controllers/page_controller_test.exs
+++ test/scrawled_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ScrawledWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Michael Lee"
   end
 end
